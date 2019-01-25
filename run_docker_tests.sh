7481d2ff08f47ae1a8ece0e57c8256ce524dd101
Ray Zimmerman <rz10@cornell.edu>
2019-01-25 11:30:55 -0600

    Minimal non-working example.

--- run_docker_tests.sh
+++ run_docker_tests.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-echo "Running run_docker_tests.sh"
-if [ 0 = 1 ]; then
-  echo "Tests passed!"
-  exit 0
-else
-  echo "Tests failed!"
-  exit 1
-fi
-
-# octave-cli --eval 'mpver'
-# octave-cli --eval 'exit(1)'
+echo "Running run_docker_tests.sh FAILED"
+exit 1
