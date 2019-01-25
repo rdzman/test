23557c60e972f5695a0c6422c87d94c807e5d017
Ray Zimmerman <rz10@cornell.edu>
2019-01-25 10:24:42 -0600

    Another attempt at getting Docker Hub tests to run.

--- run_docker_tests.sh
+++ run_docker_tests.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+echo "Running run_docker_tests.sh"
+if [ 0 ]
+then
+  echo "Tests passed!"
+  exit 0
+else
+  echo "Tests failed!"
+  exit 1
+fi
+
 # octave-cli --eval 'mpver'
-octave-cli --eval 'exit(1)'
+# octave-cli --eval 'exit(1)'
