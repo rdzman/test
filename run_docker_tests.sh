ce1e974132461a1a657890aad970de9a2a9ff4c6
Ray Zimmerman <rz10@cornell.edu>
2019-06-25 13:26:46 -0500

    Attempt to get Docker Hub auto-build/test working properly.

--- run_docker_tests.sh
+++ run_docker_tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Running run_docker_tests.sh - successfully FAILED"
-exit 1
+#echo "Running run_docker_tests.sh - successfully FAILED"
+#exit 1
+echo "Running run_docker_tests.sh - successfully PASSED"
+exit 0
